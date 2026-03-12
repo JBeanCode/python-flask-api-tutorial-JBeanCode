bf236d10c81fa927dfaec891835bf9bd6dd18f0a
JBeanCode <js02bean@gmail.com>
2026-03-12 00:56:11 +0000

    Babys-first-API

---
+++ src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+from flask import request
+
+app = Flask(__name__)
+
+todos = [ { "label": "My first task", "done": False },
+         { "label": "walk the dog", "done": False },
+         { "label": "paint the barn", "done": False } ]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    request.get_json(force=True)
+    print("Incoming request with the following body", request_body)
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
+    todos.pop(position)
+    return jsonify(todos)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
